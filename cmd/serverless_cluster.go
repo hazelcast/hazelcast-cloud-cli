efa1c244427fa16a7e25e60377bf0a492b5b60cf
Sergey Dreger <sergey.dreger@hazelcast.com>
2022-04-27 17:27:25 +0300

    HZC-2715: Fix the 'devModeEnabled' flag handling for the 'serverless' cluster creation

--- cmd/serverless_cluster.go
+++ cmd/serverless_cluster.go
@@ -14,18 +14,24 @@ func newServerlessClusterCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:     "serverless-cluster",
 		Aliases: []string{"slc"},
-		Short:   "This command allows you to make actions on your serverless clusters like: create, delete, stop or resume.",
+		Short:   "This command allows you to make actions on your serverless clusters like: create, delete, get, list, stop or resume.",
 	}
 }
 
 func newServerlessClusterCreateCmd() *cobra.Command {
 	var createClusterInputParams models.CreateServerlessClusterInput
+	var devModeEnabled bool
 
 	serverlessClusterCreateCmd := cobra.Command{
 		Use:     "create",
-		Short:   "This command allows you to create a serverless cluster.",
+		Short:   "This command allows you to create a serverless Hazelcast cluster.",
 		Example: "hzcloud serverless-cluster create --name=mycluster --region=us-west-2",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if devModeEnabled {
+				createClusterInputParams.ClusterType = models.Devmode
+			} else {
+				createClusterInputParams.ClusterType = models.Serverless
+			}
 			client := internal.NewClient()
 			cluster := internal.Validate(client.ServerlessCluster.Create(context.Background(), &createClusterInputParams)).(*models.Cluster)
 			color.Green("Cluster %s is creating. You can check the status using hzcloud serverless-cluster list.", cluster.Id)
@@ -39,20 +45,15 @@ func newServerlessClusterCreateCmd() *cobra.Command {
 	serverlessClusterCreateCmd.Flags().StringVar(&createClusterInputParams.Region, "region", "", "name of the region (required)")
 	_ = serverlessClusterCreateCmd.MarkFlagRequired("region")
 
-	var devModeEnabled bool
 	serverlessClusterCreateCmd.Flags().BoolVar(&devModeEnabled, "dev-mode-enabled", false, "development mode")
-	if devModeEnabled {
-		createClusterInputParams.ClusterType = models.Devmode
-	} else {
-		createClusterInputParams.ClusterType = models.Serverless
-	}
+
 	return &serverlessClusterCreateCmd
 }
 
 func newServerlessClusterListCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:     "list",
-		Short:   "This command allows you to create a serverless cluster.",
+		Short:   "This command allows you to create a serverless Hazelcast cluster.",
 		Example: "hzcloud serverless-cluster list",
 		Run: func(cmd *cobra.Command, args []string) {
 			client := internal.NewClient()
@@ -126,7 +127,7 @@ func newServerlessClusterStopCmd() *cobra.Command {
 
 	serverlessClusterStopCmd := &cobra.Command{
 		Use:     "stop",
-		Short:   "This command allows you to stop a Hazelcast cluster.",
+		Short:   "This command allows you to stop a serverless Hazelcast cluster.",
 		Example: "hzcloud serverless-cluster stop --cluster-id=100",
 		Run: func(cmd *cobra.Command, args []string) {
 			client := internal.NewClient()
@@ -148,7 +149,7 @@ func newServerlessClusterResumeCmd() *cobra.Command {
 
 	serverlessClusterResumeCmd := &cobra.Command{
 		Use:     "resume",
-		Short:   "This command resumes Hazelcast Instance according to its id",
+		Short:   "This command allows you to resume a serverless Hazelcast cluster.",
 		Example: "hzcloud serverless-cluster resume --cluster-id=100",
 		Run: func(cmd *cobra.Command, args []string) {
 			client := internal.NewClient()
